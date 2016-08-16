d194a619005b04b85ccbcaf139c74ac2c209f693
Nero119 <theo.crawford1992@gmail.com>
2016-08-17 02:22:25 +1000

    Small ammount of enemy AI code
    This combat code is purely for testing. This will be worked on at a
    later date!

---
+++ Assets/Code/AI/Enemy/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray sight = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(sight, out hit, 1f))
+        {
+            if(hit.collider.tag== "Player")
+            {
+                hit.transform.gameObject.GetComponent<Character>().player.AddToCurVitals(0, -5);
+            }
+        }
+	}
+}
