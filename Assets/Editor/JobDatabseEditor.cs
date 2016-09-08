d4c87831d2d244721b5a99bd8e6ba2cbe57282f5
Nero119 <theo.crawford1992@gmail.com>
2016-09-09 03:16:10 +1000

    Editor upgrades
    Fixed an issue in the Job Database system where it wasn't creating the
    database!

--- Assets/Editor/JobDatabseEditor.cs
+++ Assets/Editor/JobDatabseEditor.cs
@@ -2,15 +2,33 @@
 using UnityEditor;
 using System.Collections;
 
-namespace GG.Editor
+namespace GG.CharacterSystem.Editor
 {
     public class JobDatabseEditor : EditorWindow
     {
+        //variables
+        private JobsDatabase _classDb;
+        //constants
+        const string DATABASE_NAME = @"GGJobDatabase.asset";
+        const string DATABASE_FOLDER_NAME = @"Database";
+        const string DATABASE_FULL_PATH = @"Assets/" + DATABASE_FOLDER_NAME;
+
         [MenuItem("Horizon Guild/Database/Classes Editor")]
         static void Init()
         {
             JobDatabseEditor window = EditorWindow.GetWindow<JobDatabseEditor>();
             window.titleContent = new GUIContent("Classes Editor");
+        }
+        void OnEnable()
+        {
+            _classDb = JobsDatabase.GetDatabase<JobsDatabase>(DATABASE_FOLDER_NAME, DATABASE_NAME);
+        }
+        void onGUI()
+        {
+            if(_classDb == null)
+            {
+                Debug.LogError("You done fucked up, son");
+            }
         }
     }
 }
