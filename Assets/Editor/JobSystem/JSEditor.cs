57394fe43bea9c00bd919d75a55c2ae738a350cb
Nero119 <theo.crawford1992@gmail.com>
2016-09-10 11:51:37 +1000

    Re-designing the editor form the ground up
    Okay... here comes a theory test

---
+++ Assets/Editor/JobSystem/JSEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace GG.CharacterSystem.Editor{
+    public class JSEditor : EditorWindow
+    {
+        private JobsDatabase _jobDb;
+
+        [MenuItem("Horizon Guild/Database/Class Database")]
+        static void Init()
+        {
+            JSEditor window = EditorWindow.GetWindow<JSEditor>();
+            window.titleContent = new GUIContent("Job Editor");
+            window.Show();
+        }
+
+        #region Non Unity Functions
+       void SideBar()
+        {
+
+        }
+        void KeyEditor()
+        {
+
+        }
+        #endregion
+    }
+}
