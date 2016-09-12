e2329daa6a8d79ecf38b67347de83bafdb966958
Nero119 <theo.crawford1992@gmail.com>
2016-09-13 09:50:06 +1000

    Revert "Breaking"
    This reverts commit b7663354c35387678a4af3fd706e6277a53e03f2.

--- Assets/Code/Charcters/Character.cs
+++ Assets/Code/Charcters/Character.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Linq;
+using System;
 using System.Collections;
 using GG.CharacterSystem;
 
@@ -8,17 +8,5 @@ public class Character:MonoBehaviour
 	public BaseCharacter player = new BaseCharacter();
     public bool _isPlayer = false;
     private string guildName = string.Empty;
-
-    void Awake()
-    {
-        if(player.job.Count == 0)
-        {
-            for(int i = 0; i < JobsDatabase.GetDatabase<JobsDatabase>(@"Database", @"JobDatabase.asset").Count; i++)
-            {
-                player.job.Add(JobsDatabase.GetDatabase<JobsDatabase>(@"Database", @"JobDatabase.asset").Get(i));
-            }
-            player.job.ElementAt(player.curJob).ActivateJob();
-        }
-    }
 
 }
