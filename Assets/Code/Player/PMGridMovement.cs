d951e7222bb8313b377dfc433079e3eda3c8e41d
Nero119 <theo.crawford1992@gmail.com>
2016-10-02 11:51:16 +1000

    Added Grid Movement
    Oh, I am so no looking forwards to this

---
+++ Assets/Code/Player/PMGridMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PMGridMovement : MonoBehaviour {
+    private float _speed = 2;
+    private Vector3 _pos;
+    private Transform _tr;
+	// Use this for initialization
+	void Start () {
+        _pos = transform.position;
+        _tr = transform;
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+        {
+            _pos += Vector3.right;
+        }
+        if(Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+        {
+            _pos += Vector3.left;
+        }
+        if(Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+        {
+            _pos += Vector3.up;
+        }
+        if(Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+        {
+            _pos += Vector3.back;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+
+    }
+}
