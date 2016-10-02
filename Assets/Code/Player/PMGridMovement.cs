7f5e280e3020d934d4446ed120aba6e6513f171d
Nero119 <theo.crawford1992@gmail.com>
2016-10-02 12:36:30 +1000

    Added rotations
    Needs to be worked on
    Modified the scene

--- Assets/Code/Player/PMGridMovement.cs
+++ Assets/Code/Player/PMGridMovement.cs
@@ -5,13 +5,16 @@ public class PMGridMovement : MonoBehaviour {
     private float _speed = 2;
     private Vector3 _pos;
     private Transform _tr;
+    private Quaternion _rot;
 	// Use this for initialization
 	void Start () {
         _pos = transform.position;
         _tr = transform;
+        _rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
 	}
 	// Update is called once per frame
 	void Update () {
+        #region Movement
         if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
         {
             _pos += Vector3.right;
@@ -22,14 +25,30 @@ public class PMGridMovement : MonoBehaviour {
         }
         if(Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
         {
-            _pos += Vector3.up;
+            _pos += Vector3.forward;
         }
         if(Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
         {
             _pos += Vector3.back;
         }
-
-        transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+        #endregion
+        /*
+        #region Rotate Camera
+        if (Input.GetKey(KeyCode.Q) && _tr.rotation == _rot)
+        {
+            _rot.y += 90;
+        }
+        if(Input.GetKey(KeyCode.E) && _tr.rotation == _rot)
+        {
+            _rot.y -= 90;
+        }
+        #endregion
+        */
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, Time.deltaTime * _speed);
+        if (_tr.rotation == _rot)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+        }
 
     }
 }
