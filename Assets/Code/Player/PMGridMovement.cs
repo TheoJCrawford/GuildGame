246ea1aca2b54aa5a3f93188e4236cf247b5583a
Nero119 <theo.crawford1992@gmail.com>
2016-10-03 11:09:09 +1000

    Testing out some functionality
    Double checking on some things

--- Assets/Code/Player/PMGridMovement.cs
+++ Assets/Code/Player/PMGridMovement.cs
@@ -6,49 +6,73 @@ public class PMGridMovement : MonoBehaviour {
     private Vector3 _pos;
     private Transform _tr;
     private Quaternion _rot;
+    private float _rotationVal;
+    public string _facing;
 	// Use this for initialization
 	void Start () {
         _pos = transform.position;
         _tr = transform;
         _rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
-	}
+        GetDirection();
+    }
 	// Update is called once per frame
 	void Update () {
         #region Movement
-        if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
-        {
-            _pos += Vector3.forward;
-        }
-        if(Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
-        {
-            _pos += Vector3.back;
-        }
-        if(Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
-        {
-            _pos += Vector3.right;
-        }
-        if(Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
-        {
-            _pos += Vector3.left;
-        }
+        MovementMotor();
         #endregion
-        /*
-        #region Rotate Camera
-        if (Input.GetKey(KeyCode.Q) && _tr.rotation == _rot)
-        {
-            _rot.y += 90;
-        }
-        if(Input.GetKey(KeyCode.E) && _tr.rotation == _rot)
-        {
-            _rot.y -= 90;
-        }
-        #endregion
-        */
-        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, Time.deltaTime * _speed);
-        if (_tr.rotation == _rot)
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _pos, Time.deltaTime * _speed);
-        }
+        transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+        
 
+    }
+    void GetDirection() {
+        if(transform.rotation.y == 0)
+        {
+            _facing = "North";
+        }
+        else if(transform.rotation.y == 90)
+        {
+            _facing = "East";
+        }
+        else if(transform.rotation.y == 180 || transform.rotation.y == -180)
+        {
+            _facing = "South";
+        }
+        if(transform.rotation.y == 270 || transform.rotation.y == -270)
+        {
+            _facing = "West";
+        }
+    }
+    void MovementMotor()
+    {
+        switch (_facing)
+        {
+            case "North":
+                if(Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                {
+                    _pos += Vector3.forward;
+                }
+                if(Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                {
+                    _pos += Vector3.back;
+                }
+                break;
+            case "East":
+
+                break;
+            case "South":
+                if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                {
+                    _pos += Vector3.back;
+                }
+                if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                {
+                    _pos += Vector3.forward;
+                }
+                    break;
+            case "West":
+
+                break;
+
+        }
     }
 }
