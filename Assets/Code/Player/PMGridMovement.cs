568f923d63542d5a4b7a5f5fcc0130908506f287
Nero119 <theo.crawford1992@gmail.com>
2016-10-03 16:53:51 +1000

    Moevement Tweaks
    Fixed the movement

--- Assets/Code/Player/PMGridMovement.cs
+++ Assets/Code/Player/PMGridMovement.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class PMGridMovement : MonoBehaviour {
-    private float _speed = 3;
+    public float _speed = 3;
+    public float rotSpeed = 10;
     private Vector3 _pos;
     private Transform _tr;
     private Quaternion _rot;
     private float _rotationVal;
-    public string _facing;
+    private string _facing;
 	// Use this for initialization
 	void Start () {
         _pos = transform.position;
@@ -21,13 +22,8 @@ public class PMGridMovement : MonoBehaviour {
         RotateDirection();
         MovementMotor();
         #endregion
-        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, _speed * Time.deltaTime);
-        if (_tr.rotation == _rot)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
-        }
-        
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, rotSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
     }
     void GetDirection() {
         if(_tr.rotation.y == 0)
@@ -37,7 +33,7 @@ public class PMGridMovement : MonoBehaviour {
         }
         else if(transform.rotation.y == 90 || transform.rotation.y == -270)
         {
-            _facing = "Eastern";
+            _facing = "East";
             Debug.LogWarning("I am seen");
         }
         else if (transform.rotation.y == 180 || transform.rotation.y == -180)
@@ -53,105 +49,100 @@ public class PMGridMovement : MonoBehaviour {
     }
     void RotateDirection()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             switch (_facing)
             {
                 case "North":
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-
                     _rot = Quaternion.LookRotation(Vector3.left);
                     _facing = "West";
-                }
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    _rot = Quaternion.LookRotation(Vector3.right);
-                    _facing = "Eastern";
-                }
-                break;
-            case "Eastern":
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-                    _rot = Quaternion.LookRotation(Vector3.forward);
-                    _facing = "North";
-                }
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    _rot = Quaternion.LookRotation(Vector3.back);
-                }_facing = "South";
                     break;
-            case "South":
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-                    _rot = Quaternion.LookRotation(Vector3.right);
-                    _facing = "Eastern";
-                }
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    _rot = Quaternion.LookRotation(Vector3.left);
-                    _facing = "West";
-                }
-                break;
-            case "West":
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
+                case "West":
                     _rot = Quaternion.LookRotation(Vector3.back);
                     _facing = "South";
-                }
-                if (Input.GetKeyDown(KeyCode.E))
-                {
+                    break;
+                case "South":
+                    _rot = Quaternion.LookRotation(Vector3.right);
+                    _facing = "East";
+                    break;
+                case "East":
                     _rot = Quaternion.LookRotation(Vector3.forward);
                     _facing = "North";
+                    break;
                 }
-                break;
 
             }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            switch (_facing)
+            {
+                case "North":
+                    _rot = Quaternion.LookRotation(Vector3.right);
+                    _facing = "East";
+                    break;
+                case "East":
+                    _rot = Quaternion.LookRotation(Vector3.back);
+                    _facing = "South";
+                    break;
+                case "South":
+                    _rot = Quaternion.LookRotation(Vector3.left);
+                    _facing = "West";
+                    break;
+                case "West":
+                    _rot = Quaternion.LookRotation(Vector3.forward);
+                    _facing = "North";
+                    break;
+            }
+        }
     }
     void MovementMotor()
     {
-        switch (_facing)
+        if (_tr.rotation == _rot)
         {
-            case "North":
-                if(Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
-                {
-                    _pos += Vector3.forward;
-                }
-                if(Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
-                {
-                    _pos += Vector3.back;
-                }
-                break;
-            case "Eastern":
-                if(Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
-                {
-                    _pos += Vector3.forward;
-                }
-                if(Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
-                {
-
-                }
-                if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
-                {
-
-                }
-                if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
-                {
-
-                }
+            switch (_facing)
+            {
+                case "North":
+                    if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.forward;
+                    }
+                    if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.back;
+                    }
                     break;
-            case "South":
-                if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
-                {
-                    _pos += Vector3.back;
-                }
-                if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
-                {
-                    _pos += Vector3.forward;
-                }
+                case "East":
+                    if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.right;
+                    }
+                    if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.left;
+                    }
                     break;
-            case "West":
+                case "South":
+                    if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.back;
+                    }
+                    if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.forward;
+                    }
+                    break;
+                case "West":
+                    if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.left;
+                    }
+                    if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                    {
+                        _pos += Vector3.right;
+                    }
+                    break;
 
-                break;
-
+            }
         }
     }
     
