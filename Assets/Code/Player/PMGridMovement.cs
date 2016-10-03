3422e936a32e2dc4a2c8cb134ed7d510ce929e9d
Nero119 <theo.crawford1992@gmail.com>
2016-10-04 02:23:34 +1000

    Modifications

--- Assets/Code/Player/PMGridMovement.cs
+++ Assets/Code/Player/PMGridMovement.cs
@@ -1,37 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PMGridMovement : MonoBehaviour {
+public class PMGridMovement : MonoBehaviour
+{
+    #region Variables
     public float _speed = 3;
-    public float rotSpeed = 10;
+    public float rotSpeed = 20;
+    public bool canMove = true;
+
     private Vector3 _pos;
     private Transform _tr;
     private Quaternion _rot;
-    private float _rotationVal;
     private string _facing;
-	// Use this for initialization
-	void Start () {
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
         _pos = transform.position;
         _tr = transform;
         _rot = transform.rotation;
         GetDirection();
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         #region Movement
         RotateDirection();
-        MovementMotor();
+        if (canMove)
+        {
+            MovementMotor();
+        }
         #endregion
         transform.rotation = Quaternion.Lerp(transform.rotation, _rot, rotSpeed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
     }
-    void GetDirection() {
-        if(_tr.rotation.y == 0)
+    #region Non-Unity Functions
+    void GetDirection()
+    {
+        if (_tr.rotation.y == 0)
         {
             _facing = "North";
             Debug.LogWarning("I am seen");
         }
-        else if(transform.rotation.y == 90 || transform.rotation.y == -270)
+        else if (transform.rotation.y == 90 || transform.rotation.y == -270)
         {
             _facing = "East";
             Debug.LogWarning("I am seen");
@@ -69,9 +80,9 @@ public class PMGridMovement : MonoBehaviour {
                     _rot = Quaternion.LookRotation(Vector3.forward);
                     _facing = "North";
                     break;
-                }
-
             }
+
+        }
         if (Input.GetKeyDown(KeyCode.E))
         {
             switch (_facing)
@@ -104,46 +115,127 @@ public class PMGridMovement : MonoBehaviour {
                 case "North":
                     if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.forward;
+                        if (Physics.Raycast(_tr.position, Vector3.forward, .5f) == false)
+                        {
+                            _pos += Vector3.forward;
+                        }
                     }
                     if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.back;
+                        if (Physics.Raycast(_tr.position, Vector3.back, .5f) == false)
+                        {
+                            _pos += Vector3.back;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.right, .5f) == false)
+                        {
+                            _pos += Vector3.right;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.left, .5f) == false)
+                        {
+                            _pos += Vector3.left;
+                        }
                     }
                     break;
                 case "East":
                     if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.right;
+                        if (Physics.Raycast(_tr.position, Vector3.right, .5f) == false)
+                        {
+                            _pos += Vector3.right;
+                        }
                     }
                     if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.left;
+                        if (Physics.Raycast(_tr.position, Vector3.left, .5f) == false)
+                        {
+                            _pos += Vector3.left;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.back, .5f) == false)
+                        {
+                            _pos += Vector3.back;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.up, .5f) == false)
+                        {
+                            _pos += Vector3.up;
+                        }
                     }
                     break;
                 case "South":
                     if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.back;
+                        if (Physics.Raycast(_tr.position, Vector3.back, .5f) == false)
+                        {
+                            _pos += Vector3.back;
+                        }
                     }
                     if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.forward;
+                        if (Physics.Raycast(_tr.position, Vector3.forward, .5f) == false)
+                        {
+                            _pos += Vector3.forward;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.left, .5f) == false)
+                        {
+                            _pos += Vector3.left;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.right, .5f) == false)
+                        {
+                            _pos += Vector3.right;
+                        }
                     }
                     break;
                 case "West":
                     if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.left;
+                        if (Physics.Raycast(_tr.position, Vector3.left, .5f) == false)
+                        {
+                            _pos += Vector3.left;
+                        }
                     }
                     if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
                     {
-                        _pos += Vector3.right;
+                        if (Physics.Raycast(_tr.position, Vector3.right, .5f) == false)
+                        {
+                            _pos += Vector3.right;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.forward, .5f) == false)
+                        {
+                            _pos += Vector3.forward;
+                        }
+                    }
+                    if (Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+                    {
+                        if (Physics.Raycast(_tr.position, Vector3.left, .5f) == false)
+                        {
+                            _pos += Vector3.left;
+                        }
                     }
                     break;
 
             }
         }
     }
-    
+
+    #endregion
 }
