2ce657b3c867c640080780a9e560d68e46013ed2
Nero119 <theo.crawford1992@gmail.com>
2016-10-03 15:37:24 +1000

    Movement tweaks

--- Assets/Code/Player/PMGridMovement.cs
+++ Assets/Code/Player/PMGridMovement.cs
@@ -12,35 +12,99 @@ public class PMGridMovement : MonoBehaviour {
 	void Start () {
         _pos = transform.position;
         _tr = transform;
-        _rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+        _rot = transform.rotation;
         GetDirection();
     }
 	// Update is called once per frame
 	void Update () {
         #region Movement
+        RotateDirection();
         MovementMotor();
         #endregion
-        transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, _speed * Time.deltaTime);
+        if (_tr.rotation == _rot)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pos, Time.deltaTime * _speed);
+        }
         
 
     }
     void GetDirection() {
-        if(transform.rotation.y == 0)
+        if(_tr.rotation.y == 0)
         {
             _facing = "North";
+            Debug.LogWarning("I am seen");
         }
-        else if(transform.rotation.y == 90)
+        else if(transform.rotation.y == 90 || transform.rotation.y == -270)
         {
-            _facing = "East";
+            _facing = "Eastern";
+            Debug.LogWarning("I am seen");
         }
-        else if(transform.rotation.y == 180 || transform.rotation.y == -180)
+        else if (transform.rotation.y == 180 || transform.rotation.y == -180)
         {
             _facing = "South";
+            Debug.LogWarning("I am seen");
         }
-        if(transform.rotation.y == 270 || transform.rotation.y == -270)
+        else if (transform.rotation.y == 270 || transform.rotation.y == -90)
         {
             _facing = "West";
+            Debug.LogWarning("I am seen");
         }
+    }
+    void RotateDirection()
+    {
+            switch (_facing)
+            {
+                case "North":
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+
+                    _rot = Quaternion.LookRotation(Vector3.left);
+                    _facing = "West";
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.right);
+                    _facing = "Eastern";
+                }
+                break;
+            case "Eastern":
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.forward);
+                    _facing = "North";
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.back);
+                }_facing = "South";
+                    break;
+            case "South":
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.right);
+                    _facing = "Eastern";
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.left);
+                    _facing = "West";
+                }
+                break;
+            case "West":
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.back);
+                    _facing = "South";
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    _rot = Quaternion.LookRotation(Vector3.forward);
+                    _facing = "North";
+                }
+                break;
+
+            }
     }
     void MovementMotor()
     {
@@ -56,9 +120,24 @@ public class PMGridMovement : MonoBehaviour {
                     _pos += Vector3.back;
                 }
                 break;
-            case "East":
+            case "Eastern":
+                if(Input.GetAxis("Horizontal") < 0 && _tr.position == _pos)
+                {
+                    _pos += Vector3.forward;
+                }
+                if(Input.GetAxis("Horizontal") > 0 && _tr.position == _pos)
+                {
 
-                break;
+                }
+                if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
+                {
+
+                }
+                if (Input.GetAxis("Vertical") < 0 && _tr.position == _pos)
+                {
+
+                }
+                    break;
             case "South":
                 if (Input.GetAxis("Vertical") > 0 && _tr.position == _pos)
                 {
@@ -75,4 +154,5 @@ public class PMGridMovement : MonoBehaviour {
 
         }
     }
+    
 }
