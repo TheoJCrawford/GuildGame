035274fa963c0fc51a8583288dcbb8b4bf2b7017
Nero119 <theo.crawford1992@gmail.com>
2016-10-05 02:16:34 +1000

    Added the base for the editors
    To do: Go back and rename some the creaturefiles to start with CS

---
+++ Assets/Editor/Creatures/CSCreatureEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CSCreatureEditor : EditorWindow {
+
+    [MenuItem("Horizon Guild/Database/Creature Editor")]
+    public static void Init()
+    {
+
+    }
+    
+}
---
+++ Assets/Editor/Creatures/CSSpeciesEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CSSpeciesEditor : EditorWindow {
+    [MenuItem("Horizon Guild/Database/Species Editor")]
+    public static void Init()
+    {
+        CSSpeciesEditor window = EditorWindow.GetWindow<CSSpeciesEditor>();
+        window.Show();
+    }
+}
