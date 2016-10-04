2b1d47f68c44f06fb77f40f113a91f99b2aa282b
Nero119 <theo.crawford1992@gmail.com>
2016-10-05 01:54:12 +1000

    Added Creatures and Species
    As the title says. Editor will come tomorrow

---
+++ Assets/Code/Creatures/BaseCreature.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using GG.CharacterSystem;
+
+namespace GG.CreatureSystem
+{
+    public class BaseCreature
+    {
+        #region Variables
+        private string _name;
+        private string _descript;
+        private Vital _health;
+        private BaseStats[] _stats;
+        private int _attack;
+        private int _defense;
+        private int _exp;
+        private int _gold;
+        #endregion
+        #region Setters and Getters
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string descript
+        {
+            get { return _descript; }
+            set { _descript = value; }
+        }
+        public Vital health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+        public BaseStats[] stats
+        {
+            get { return _stats; }
+            set { _stats = value; }
+        }
+        public int attack
+        {
+            get { return _attack; }
+            set { _attack = value; }
+        }
+        public int defence
+        {
+            get { return _defense; }
+            set { _defense = value; }
+        }
+        public int exp
+        {
+            get { return _exp; }
+            set { _exp = value; }
+        }
+        public int gold
+        {
+            get { return _gold; }
+            set { _gold = value; }
+        }
+        #endregion
+        #region Constructors
+
+        public BaseCreature()
+        {
+            _name = "Goblin";
+            _descript = string.Empty;
+            _health = new Vital();
+            _stats = new BaseStats[6];
+            _attack = 0;
+            _defense = 0;
+            _exp = 20;
+            _gold = 15;
+        }
+        
+        #endregion
+        #region Fuction
+        #endregion
+    }
+}
---
+++ Assets/Code/Creatures/Species.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Species {
+    #region variables
+    private string _name;
+    private string _desript;
+    private Sprite _icon;
+    #endregion
+    #region
+    public string name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public string descript
+    {
+        get { return _desript; }
+        set { _desript = value; }
+    }
+    public Sprite icon
+    {
+        get { return _icon; }
+        set { _icon = value; }
+    }
+    #endregion
+    public Species()
+    {
+        _name = string.Empty;
+        _desript = string.Empty;
+        _icon = new Sprite();
+    }
+
+}
