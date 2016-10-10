3425b43e4bcb6a950ff47066702d23bee9657040
Nero119 <theo.crawford1992@gmail.com>
2016-10-10 22:46:47 +1000

    Looking at a complete creature rewrite
    Might need to work on states soonish... ANywho, the creature base class
    is almost done :smile_cat:

---
+++ Assets/Code/Creatures/ICSCreature.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GG.CreatureSystem
+{
+    public interface ICSCreature
+    {
+        string name { set; get; }
+        Sprite image { set; get; }
+        string descript { set; get; }
+        CSSpecies species { set; get; }
+        int exp { set; get; }
+        int money { set; get; }
+        int speed { set; get; }
+    }
+}
---
+++ Assets/Editor/CreatureEditor/CSCreatureEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CSCreatureEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
