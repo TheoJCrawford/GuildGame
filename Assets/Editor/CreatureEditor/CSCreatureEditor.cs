7cdb184eb0d5a1b28f22e7988cd68e1e1e2d921c
Nero119 <theo.crawford1992@gmail.com>
2016-10-27 01:14:30 +1000

    Added a selctor for the species!
    This will require an update function for when the editor is enabled to
    check the creatures in the database's species against the database so as
    to get the update the descriptions on the creature's species.

--- Assets/Editor/CreatureEditor/CSCreatureEditor.cs
+++ Assets/Editor/CreatureEditor/CSCreatureEditor.cs
@@ -7,9 +7,13 @@ namespace GG.CreatureSystem
     public class CSCreatureEditor : EditorWindow
     {
         const string DATABASE_NAME = @"CSCreatureDatabase.asset";
+        const string SPECIES_DATABASE = @"CSSpecies Database.asset";
         const string DATABASE_FOLDER = @"Database";
 
         private CSCreatureDatabase _db;
+        private CSSpeciesDatabase _speciesDb;
+        private int _selectedCreature;
+        private Vector2 _scrollPos = Vector2.zero;
 
         [MenuItem("Horizon Guild/Creature/Creature Editor")]
         private static void Init()
@@ -24,6 +28,16 @@ namespace GG.CreatureSystem
         void OnEnable()
         {
             _db = CSCreatureDatabase.GetDatabase<CSCreatureDatabase>(DATABASE_FOLDER, DATABASE_NAME);
+            EditorUtility.SetDirty(_db);
+            _speciesDb = CSSpeciesDatabase.GetDatabase<CSSpeciesDatabase>(DATABASE_FOLDER, SPECIES_DATABASE);
+            if (_db.Count > 0)
+            {
+                _selectedCreature = 0;
+            }
+            else {
+                _selectedCreature = -1;
+            }
+
         }
         void OnGUI()
         {
@@ -32,6 +46,9 @@ namespace GG.CreatureSystem
             GUILayout.EndArea();
             GUILayout.BeginArea(new Rect(0, 20, 150, 780));
             SideBar();
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(150, 20, 650, 780));
+            MainScreen();
             GUILayout.EndArea();
         }
 
@@ -49,13 +66,33 @@ namespace GG.CreatureSystem
             {
                 for(int i = 0; i < _db.Count; i++)
                 {
-                    GUILayout.Button(_db.Get(i).name, GUILayout.ExpandWidth(true));
+                    if(GUILayout.Button(_db.Get(i).name, GUILayout.ExpandWidth(true)))
+                    {
+                        _selectedCreature = i;
+                    }
                 }
             }
         }
         void MainScreen()
         {
-
+            if(_db.Count > 0)
+            {
+                //name
+                _db.Get(_selectedCreature).name = GUILayout.TextArea(_db.Get(_selectedCreature).name);
+                //Species
+                GUILayout.BeginArea(new Rect(550, 20, 100, 400));
+                GUILayout.Label(_db.Get(_selectedCreature).species.name);
+                GUILayout.BeginScrollView(_scrollPos, "Box", GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+                for (int i = 0; i < _speciesDb.Count; i++)
+                {
+                   if(GUILayout.Button(new GUIContent(_speciesDb.Get(i).name)))
+                    {
+                        _db.Get(_selectedCreature).species = _speciesDb.Get(i);
+                    }
+                }
+                GUILayout.EndArea();
+            }
+            
         }
     }
 }
