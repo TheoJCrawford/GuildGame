a0ec88656ba6c2412c2fee114986184f1e5479bc
Nero119 <theo.crawford1992@gmail.com>
2016-12-14 13:08:59 +1000

    Recallibrating UI and where scripts are located
    Yaaaay!

--- Assets/Code/UserInterface/HeadsUpDisplay.cs
+++ Assets/Code/UserInterface/HeadsUpDisplay.cs
@@ -1,32 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeadsUpDisplay : MonoBehaviour {
-    #region Variables
-    private float _maxBarWidth;
-    private float _hpBarWdith;
-    private float _barHieght = 20f;
-    #endregion
-    void Start () {
-        _maxBarWidth = Screen.width * 0.65f;
-        _hpBarWdith = _maxBarWidth;
-
-    }
-    void Update()
+namespace GG
+{
+    public class HeadsUpDisplay : MonoBehaviour
     {
-        if (Input.GetMouseButtonDown(2))
+        #region Variables
+        private Character _mainCharacter;
+        private float _maxBarWidth;
+        private float _hpBarWdith;
+        private float _barHieght = 20f;
+        #endregion
+        void Awake()
         {
-            gameObject.GetComponent<Character>().player.AddToCurVitals(0, -1);
-            AdjHealthBar();
+            _mainCharacter = GameObject.Find("PlayerCharacter").GetComponent<Character>();
         }
-    }
-	void OnGUI () {
-        GUI.Box(new Rect(5, 5, _maxBarWidth, _barHieght), gameObject.GetComponent<Character>().player.GetVitals(0).name + " :" + gameObject.GetComponent<Character>().player.GetVitals(0).curValue.ToString() + "/" + gameObject.GetComponent<Character>().player.GetVitals(0).fullValue.ToString());
-        GUI.Box(new Rect(5, 5 + _barHieght, _maxBarWidth, _barHieght), gameObject.GetComponent<Character>().player.GetVitals(1).name + " :" + gameObject.GetComponent<Character>().player.GetVitals(1).curValue.ToString() + "/" + gameObject.GetComponent<Character>().player.GetVitals(1).fullValue.ToString());
-        GUI.Box(new Rect(5, 5 + _barHieght * 2, _maxBarWidth, _barHieght), gameObject.GetComponent<Character>().player.GetVitals(2).name + " :" + gameObject.GetComponent<Character>().player.GetVitals(2).curValue.ToString() + "/" + gameObject.GetComponent<Character>().player.GetVitals(2).fullValue.ToString());
-    }
-    void AdjHealthBar()
-    {
-        _hpBarWdith = _maxBarWidth * (gameObject.GetComponent<Character>().player.GetVitals(0).curValue / gameObject.GetComponent<Character>().player.GetVitals(0).fullValue);
+        void Start()
+        {
+            _maxBarWidth = Screen.width * 0.65f;
+            _hpBarWdith = _maxBarWidth;
+
+        }
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                gameObject.GetComponent<Character>().player.AddToCurVitals(0, -1);
+                AdjHealthBar();
+            }
+        }
+        void OnGUI()
+        {
+            GUI.Box(new Rect(5, 5, _maxBarWidth, _barHieght), _mainCharacter.player.GetVitals(0).name + " :" + _mainCharacter.player.GetVitals(0).curValue.ToString() + "/" + _mainCharacter.player.GetVitals(0).fullValue.ToString());
+            GUI.Box(new Rect(5, 5 + _barHieght, _maxBarWidth, _barHieght), _mainCharacter.player.GetVitals(1).name + " :" + _mainCharacter.player.GetVitals(1).curValue.ToString() + "/" + _mainCharacter.player.GetVitals(1).fullValue.ToString());
+            GUI.Box(new Rect(5, 5 + _barHieght * 2, _maxBarWidth, _barHieght), _mainCharacter.player.GetVitals(2).name + " :" + _mainCharacter.player.GetVitals(2).curValue.ToString() + "/" + _mainCharacter.player.GetVitals(2).fullValue.ToString());
+        }
+        void AdjHealthBar()
+        {
+            _hpBarWdith = _maxBarWidth * (_mainCharacter.player.GetVitals(0).curValue / _mainCharacter.player.GetVitals(0).fullValue);
+        }
     }
 }
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static PauseMenu instance;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    public Button exitButton;
+	// Use this for initialization
+	void Awake () {
+        isPaused = false;
+        pauseMenu = GameObject.Find("PauseMenu");
+        exitButton = GameObject.Find("exitButton").GetComponent<Button>();
+        exitButton.onClick.AddListener(ButtonExitGame);
+        pauseMenu.SetActive(false);
+        if(pauseMenu == null)
+        {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                isPaused = true;
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+            }
+        }
+	}
+    public void ButtonExitGame()
+    {
+        Debug.Log("We are listening");
+        Application.Quit();
+    }
+}
