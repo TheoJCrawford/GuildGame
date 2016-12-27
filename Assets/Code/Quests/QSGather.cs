81dee63f2a1ebf29d0474efb9e79b42096b37741
Nero119 <theo.crawford1992@gmail.com>
2016-12-27 23:48:15 +1000

    Slight tweaking

---
+++ Assets/Code/Quests/QSGather.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GG.QuestSystem
+{
+    class QSGather : IQSQuest
+    {
+        #region Setters and Geters
+        public string descript
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+        public int moneyReward
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+
+        public string name
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+
+        public string questGiver
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+        public string questReciever
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+        public int questTime
+        {
+            get{throw new NotImplementedException();}
+            set{throw new NotImplementedException();}
+        }
+        #endregion
+    }
+}
